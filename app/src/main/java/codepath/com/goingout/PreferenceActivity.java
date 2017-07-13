6d9a4f3d4b257e401d8d9af448e9980ce9fe6855
Amadou Camara <acamara@college.harvard.edu>
2017-07-13 00:02:09 -0700

    Adding the beginning of the cards view in filters

--- app/src/main/java/codepath/com/goingout/PreferenceActivity.java
+++ app/src/main/java/codepath/com/goingout/PreferenceActivity.java
@@ -1,9 +1,16 @@
 package codepath.com.goingout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class PreferenceActivity extends AppCompatActivity {
+    private RecyclerView rvFilters;
+
+    private List<String> types;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
