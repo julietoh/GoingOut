37d99faaa03bd77c86e817ee9ccd20d4f030f48b
Juliet Oh <joh@fb.com>
2017-07-14 17:07:30 -0700

    Deleted unnecessary XML file

--- app/src/main/java/codepath/com/goingout/MainActivity.java
+++ app/src/main/java/codepath/com/goingout/MainActivity.java
@@ -7,7 +7,6 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
 
             Bundle inBundle = getIntent().getExtras();
             String name = inBundle.get("name").toString();
