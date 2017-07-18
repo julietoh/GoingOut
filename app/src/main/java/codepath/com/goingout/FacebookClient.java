6b76f69f49c623035216cf0be989ecf0cd37ebe2
rafaf09 <rafaf09@hotmail.com>
2017-07-18 11:43:58 -0700

    merge

--- app/src/main/java/codepath/com/goingout/FacebookClient.java
+++ app/src/main/java/codepath/com/goingout/FacebookClient.java
@@ -1,5 +1,12 @@
 package codepath.com.goingout;
 
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.api.BaseApi;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
 
 import com.evdb.javaapi.data.Event;
 
@@ -7,8 +14,30 @@ import com.evdb.javaapi.data.Event;
  * Created by joh on 7/13/17.
  */
 
-public class FacebookClient {
-    //
-    Event
-}
+public class FacebookClient extends OAuthBaseClient {
+    public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+    public static final String REST_URL = "https://graph.facebook.com/";
+    public static final String APP_ID = "274274976385926";
+    public static final String APP_SECRET = "6bb7234b44df8388039ba23c47a69be2";
 
+    // Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
+    public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
+
+    // See https://developer.chrome.com/multidevice/android/intents
+    public static final String REST_CALLBACK_URL_TEMPLATE = "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end";
+
+    public FacebookClient(Context context) {
+        super(context, REST_API_INSTANCE,
+                REST_URL,
+                APP_ID,
+                APP_SECRET,
+                String.format(REST_CALLBACK_URL_TEMPLATE, context.getString(R.string.intent_host),
+                        context.getString(R.string.intent_scheme), context.getPackageName(), FALLBACK_URL));
+    }
+
+    // DEFINE METHODS for different API endpoints here
+    public void getUserEvents(AsyncHttpResponseHandler handler) {
+
+    }
+
+}
