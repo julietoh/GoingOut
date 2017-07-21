22e7d38dc11e16c620a622ed653fc8f1e4844165
Amadou Camara <acamara@college.harvard.edu>
2017-07-20 22:46:43 -0700

    place holders for the comments page

--- app/src/main/java/codepath/com/goingout/DetailsActivity.java
+++ app/src/main/java/codepath/com/goingout/DetailsActivity.java
@@ -1,20 +1,63 @@
 package codepath.com.goingout;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.loopj.android.http.AsyncHttpClient;
+
+import java.util.ArrayList;
+
+import codepath.com.goingout.adapters.PostAdapter;
+import codepath.com.goingout.models.Post;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    //instance fields
+    AsyncHttpClient client;
+
+
+    // the list of currenly playing movies
+    ArrayList<Post> posts;
+    // the recylcler view
+    RecyclerView rvPosts;
+    //the adapter wired to the recycler view
+    PostAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //initialize the client
+        client = new AsyncHttpClient();
+        //initialize the list of movies
+        posts = new ArrayList<>();
+        //initialize the adapter -- movies array list cannot be reinitialized after this point
+        adapter = new PostAdapter(posts);
+
+        getPosts();
+
+        //resolve the recycler view and connect a layout manager and the adapter
+        rvPosts = (RecyclerView) findViewById(R.id.rvPosts);
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        rvPosts.setAdapter(adapter);
+
+
     }
 
     public void toReviews(View view) {
         Intent intent = new Intent(DetailsActivity.this, ReviewActivity.class);
         startActivity(intent);
+    }
+
+    public void getPosts() {
+        for (int i = 0; i<10; i++)
+        {
+            posts.add(new Post("Amadou","10PM", "Wow this club is awesome"));
+        }
     }
 }
