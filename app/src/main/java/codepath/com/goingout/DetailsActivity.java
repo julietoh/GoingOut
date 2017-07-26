cc9a6809418b8456dbbc3306969e36eec6381271
Juliet Oh <joh@fb.com>
2017-07-26 01:27:04 -0700

    Configured Firebase and succesfully tested sending and recieving to database

--- app/src/main/java/codepath/com/goingout/DetailsActivity.java
+++ app/src/main/java/codepath/com/goingout/DetailsActivity.java
@@ -37,6 +37,28 @@ public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        // Testing - Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        // Testing - Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d("fb", "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("fb", "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        myRef.setValue("yoooo");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
