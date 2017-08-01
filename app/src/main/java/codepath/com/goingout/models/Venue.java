45a39d2702faca648ec98774b6c379e990fe14ce
Juliet Oh <joh@fb.com>
2017-07-31 17:43:03 -0700

    Centered video in layout

--- app/src/main/java/codepath/com/goingout/models/Venue.java
+++ app/src/main/java/codepath/com/goingout/models/Venue.java
@@ -35,7 +35,7 @@ public class Venue {
         if (object.has("photos")) {
             photoRef = object.getJSONArray("photos").getJSONObject(0).getString("photo_reference");
             sb.append(photoRef);
-            sb.append("&key=AIzaSyCZkYpPiWoufjD9kTCt7golzT2fkS3duz0");
+            sb.append("&key=AIzaSyBkZ5l_wF-d7au5GtGUk9RT46fXw2mkUnM");
             finalURL = sb.toString();
         }
 
